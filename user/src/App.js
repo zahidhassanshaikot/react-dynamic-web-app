37436076616f344f02ca7d7cb1e72683d924d995
zahidhassanshaikot <zahidhassanshaikot@gmail.com>
2020-02-03 15:24:37 +0600

    home page formation

--- user/src/App.js
+++ user/src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import TopBanner from "./components/TopBanner/TopBanner";
-import Services from "./components/Services/Services";
-import TopNavigation from "./components/TopNavigation/TopNavigation";
-import Analysis from "./components/Analysis/Analysis";
-import Summary from "./components/Summary/Summary";
-import RecentProject from "./components/RecentProjects/RecentProject";
-import Courses from "./components/Courses/Courses";
-import Video from "./components/Video/Video";
-import ClientReview from "./components/ClientReview/ClientReview";
-import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage";
 
 
 function App() {
   return (
     <div>
-        <TopNavigation/>
-        <TopBanner/>
-        <Services/>
-        <Analysis/>
-        <Summary/>
-        <RecentProject/>
-        <Courses/>
-        <Video/>
-        <ClientReview/>
-        <Footer/>
+        <HomePage/>
     </div>
   );
 }
